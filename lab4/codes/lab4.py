b63602182423b5a5edabd8fd25284628c5b6d834
SunilKrSingh <sunilks5963@gmail.com>
2019-09-19 16:33:15 +0530

    added lab codes

--- lab4/codes/lab4.py
+++ lab4/codes/lab4.py
@@ -1,93 +1,103 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 12 04:15:18 2019
-
-@author: anujg
-"""
-from numpy import array
-from numpy import mean
-from numpy import cov
-from numpy.linalg import eig
-
-import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler 
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.utils import shuffle
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+import numpy as np
 
 
+def load_dataset(path_to_file):
+    return pd.read_csv(path_to_file)
 
-X=[]
-Y=[]
-for i in range(1000):
-    x, y = np.random.multivariate_normal([0,0],[[7,10],[10,18]],1).T
-    X=X+list(x)
-    Y=Y+list(y)
+def normalize(df):
+    scaler = MinMaxScaler()
+    arrNorm = scaler.fit_transform(df)
+    dfNorm = pd.DataFrame(arrNorm)
+    return dfNorm
 
-C=[]
-for i in range(len(X)):
-    C.append([X[i],Y[i]])
-C=np.array(C)
+def standardize(df):
+    scaler = StandardScaler()
+    Std = scaler.fit_transform(df)
+    dfStd = pd.DataFrame(Std)
+    return dfStd
 
-plt.scatter(C.T[0],C.T[1],marker='x')
+def shuffle_df(df):
+    return shuffle(df)
 
-# calculate covariance matrix of centered matrix
-V = cov(C.T)
-print(V)
-
-# eigen decomposition of covariance matrix
-values, vectors = eig(V)
-print(vectors)
-print(values)
-qt=vectors.transpose()
-plt.quiver(qt[0][0],qt[0][1],scale=5,color='r')
-plt.quiver(qt[1][0],qt[1][1],scale=5,color='r')
-
-# project data
-P = vectors.T.dot(C.T)     #project on both eigen vectors
-D1=vectors.T[0].dot(C.T)   #project on eigen vector 1
-D2=vectors.T[1].dot(C.T)   #project on eigen vector 2
-
-#reconstructing the data
-D0p=[]     #from both vectors
-D1p=[]     #from eigen vector 1
-D2p=[]     #from eigen vector 2
-for i in range(1000):
-    D1p.append(D1[i]*vectors.T[0])
-    D2p.append(D2[i]*vectors.T[1])
-    D0p.append(np.add(P.T[i][0]*vectors.T[0],P.T[i][1]*vectors.T[1]))
-
-D1p=np.array(D1p).transpose()
-D2p=np.array(D2p).transpose()
-  
-#projecting data in one dimension
-plt.scatter(D1p[0],D1p[1],marker='x')    #along vector 1
-plt.scatter(D2p[0],D2p[1],marker='x')    #along vector 2
-
-
-plt.xlim([-25,20])
-plt.ylim([-15,15])
-
-
-
-#finding error in reconstructed data and original data
-E1=[]
-E2=[]
-E0=[]
-
-#finding the eucledian distance
-for i in range(1000):
-    e0=((C[i][0]-D0p[i][0])**2+(C[i][1]-D0p[i][1])**2)
-    e1=((C[i][0]-D1p.T[i][0])**2+(C[i][1]-D1p.T[i][1])**2)
-    e2=((C[i][0]-D2p.T[i][0])**2+(C[i][1]-D2p.T[i][1])**2)     
-    E0.append(e0)
-    E1.append(e1)    
-    E2.append(e2)
-
-error0=sum(E0)**0.5  #error when both vectors are used 
-error1=sum(E1)**0.5  #error when vector 1 is used
-error2=sum(E2)**0.5  #error when vector 2 is used  
-
-
-#saving original and reduced data
-np.savetxt("original.csv",C , delimiter=",")
-np.savetxt("D1.csv",D1, delimiter=",")
-np.savetxt("D2.csv",D2, delimiter=",")
+def test_train_split(df):
+    X = df.iloc[:, :-1].values
+    y = df.iloc[:,-1].values
+    #X = np.array(df.iloc[:, 0:-1])
+    #y = np.array(df['Z_Scratch'])
     
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
+    np.savetxt("/home/sunil/Desktop/sem3/ds3/data_science_3/lab5/X_train.csv",X_train,delimiter=',')
+    np.savetxt("/home/sunil/Desktop/sem3/ds3/data_science_3/lab5/X_test.csv",X_test,delimiter=',')
+    np.savetxt("/home/sunil/Desktop/sem3/ds3/data_science_3/lab5/y_train.csv",y_train,delimiter=',')
+    np.savetxt("/home/sunil/Desktop/sem3/ds3/data_science_3/lab5/y_test.csv",y_test,delimiter=',')
+    print(type(X_train))
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(X_train, y_train)
+    pred = knn.predict(X_test)
+    accuracy = (accuracy_score(y_test, pred))
+    print(accuracy)
+    
+    print(confusion_matrix(y_test, pred))
+def classification(k):
+    X_train = np.array(load_dataset("/home/sunil/Desktop/sem3/ds3/data_science_3/lab5/X_train.csv"))
+    X_test = np.array(load_dataset("/home/sunil/Desktop/sem3/ds3/data_science_3/lab5/X_test.csv"))
+    y_train = np.array(load_dataset("/home/sunil/Desktop/sem3/ds3/data_science_3/lab5/y_train.csv"))
+    y_test = np.array(load_dataset("/home/sunil/Desktop/sem3/ds3/data_science_3/lab5/y_test.csv"))
+    
+    accuracies = []
+    for i in range(len(k)):
+        knn = KNeighborsClassifier(n_neighbors=k[i])
+        knn.fit(X_train, y_train)
+        pred = knn.predict(X_test)
+        accuracy = (accuracy_score(y_test, pred))
+        accuracies.append(accuracy)
+        print("k = ",k[i]," : ")
+        print(confusion_matrix(y_test, pred))
+    return accuracies
+
+def main():
+    path_to_file = "/home/sunil/Desktop/sem3/ds3/data_science_3/lab5/inLab/SteelPlateFaults-2class.csv"
+    df = load_dataset(path_to_file)
+    
+    dfN = df.copy()
+    dfS = df.copy()
+    
+    k = [1, 3, 5, 7, 9, 11, 13, 15, 17, 21]
+    
+    test_train_split(df)
+    dfAcc = classification(k)
+    
+    dfNorm = normalize(dfN)
+    test_train_split(dfNorm)
+    #dfNormAcc = classification(k)
+    
+    dfStd = standardize(dfS)
+    test_train_split(dfStd)
+    #dfStdAcc = classification(k)
+    
+    print()
+    print("accuracy matrix for Original Data: \n",dfAcc)
+    print()
+    #print("accuracy matrix for Normalized Data: \n",dfNormAcc)
+    print()
+    #print("accuracy matrix for Standardized Data: \n",dfStdAcc)
+    
+    plt.plot(k,dfAcc,label = "org")
+    #plt.plot(k,dfNormAcc,label = "norm")
+    #plt.plot(k,dfStdAcc,label = "standard")
+    plt.legend()
+    plt.show()
+    
+if __name__ == "__main__":
+    main()
